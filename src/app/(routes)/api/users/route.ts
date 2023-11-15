95c39fb0f3cdaa1b32a98da1b0c1db03da6c52a3
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-11-15 10:02:43 +0700

    feat: add route users

---
+++ src/app/(routes)/api/users/route.ts
@@ -0,0 +1,10 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/libs/prisma";
+
+export const GET = async (req: NextRequest) => {
+  const users = await prisma.user.findMany()
+
+  return NextResponse.json({
+    users
+  }, { status: 200 })
+}
